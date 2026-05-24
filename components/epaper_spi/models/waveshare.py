32c27aa8401af0ec8127dac826585e9c33aa5c43
MooseAde <mooseade@outlook.com>
2026-05-24 17:27:06 +0100

    Remove iterable flag from int

--- components/epaper_spi/models/waveshare.py
+++ components/epaper_spi/models/waveshare.py
@@ -35,7 +35,7 @@ class WaveshareModel(EpaperModel):
         else:
             initsequencefull = self.initsequencefull
             initsequencefull_length = len(self.initsequencefull)
-        return *lut, *lut_partial, *initsequencefull, *initsequencefull_length
+        return *lut, *lut_partial, *initsequencefull, initsequencefull_length
     
 # fmt: off
 WaveshareModel(
