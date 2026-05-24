b1d1d600c4dfc448e2da8327cd97e7e02cd8bb01
MooseAde <mooseade@outlook.com>
2026-05-24 20:41:48 +0100

    Direct send commands to ensure correct order

--- components/epaper_spi/epaper_waveshare.cpp
+++ components/epaper_spi/epaper_waveshare.cpp
@@ -8,18 +8,25 @@ bool EpaperWaveshare::initialise(bool partial) {
 
   if (partial) {
     // Send partial init sequence, as defined in waveharemodel.initsequence property
-    EPaperBase::initialise(partial);
+    // EPaperBase::initialise(partial);
     this->cmd_data(0x32, this->partial_lut_, this->partial_lut_length_);
+    this->cmd_data(0x37, {0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00});
+    this->cmd_data(0x11, {0x03});  // Set data entry mode to Y increment, X increment
+    this->cmd_data(0x44, {0x01, (uint8_t) ((this->width_ % 8 == 0)? (this->width_ / 8 ): (this->width_ / 8 + 1))}); // Set RAM X start/end
+    this->cmd_data(0x45, {(uint8_t) 0, (uint8_t) 0, (uint8_t) (this->height_ & 0xFF), (uint8_t) ((this->height_ & 0x100) >> 8)});  // Set RAM Y start/end 
     this->cmd_data(0x3C, {0x80});
     this->cmd_data(0x22, {0xCf});
     this->command(0x20);
     this->next_delay_ = 100;
   } else {
     // Send Full init sequence as defined in waveharemodel.initsequencefull property
-    // EPaperBase::send_init_sequence_(this->initsequencefull_, this->initsequencefull_length_);
-    EPaperBase::initialise(partial);
-    // Full Init does not require sending the LUT
-    // this->cmd_data(0x32, this->lut_, this->lut_length_);
+    // SEE commented code below from 2.66in example
+    if (EPaperBase::reset()) {
+      this->command(0x12);
+    }
+    this->cmd_data(0x11, {0x03});  // Set data entry mode to Y increment, X increment
+    this->cmd_data(0x44, {0x01, (uint8_t) ((this->width_ % 8 == 0)? (this->width_ / 8 ): (this->width_ / 8 + 1))}); // Set RAM X start/end
+    this->cmd_data(0x45, {(uint8_t) 0, (uint8_t) 0, (uint8_t) (this->height_ & 0xFF), (uint8_t) ((this->height_ & 0x100) >> 8)});  // Set RAM Y start/end
     this->cmd_data(0x3C, {0x01});
   }
   this->send_red_ = true;
@@ -52,3 +59,79 @@ void EpaperWaveshare::refresh_screen(bool partial) {
 
 void EpaperWaveshare::deep_sleep() { this->cmd_data(0x10, {0x01}); }
 }  // namespace esphome::epaper_spi
+
+
+
+// void EPD_2IN66_Init(void)
+// {
+//     EPD_2IN66_Reset();
+//     EPD_2IN66_ReadBusy();
+//     EPD_2IN66_SendCommand(0x12);//soft  reset
+//     EPD_2IN66_ReadBusy();
+	
+// 	/* Y increment, X increment */
+//     EPD_2IN66_SendCommand(0x11);
+//     EPD_2IN66_SendData(0x03);
+//     /*	Set RamX-address Start/End position	*/
+//     EPD_2IN66_SendCommand(0x44);
+//     EPD_2IN66_SendData(0x01); 
+//     EPD_2IN66_SendData((EPD_2IN66_WIDTH % 8 == 0)? (EPD_2IN66_WIDTH / 8 ): (EPD_2IN66_WIDTH / 8 + 1) );
+//     /*	Set RamY-address Start/End position	*/
+//     EPD_2IN66_SendCommand(0x45);
+//     EPD_2IN66_SendData(0);
+//     EPD_2IN66_SendData(0);
+//     EPD_2IN66_SendData((EPD_2IN66_HEIGHT&0xff));
+//     EPD_2IN66_SendData((EPD_2IN66_HEIGHT&0x100)>>8);
+
+//     EPD_2IN66_SendCommand(0x3C);        // Border  设置  黑白一般设置为跟随白波形即 0x01        Border setting 
+//     EPD_2IN66_SendData(0x01);
+	
+// }
+
+// /******************************************************************************
+// function :	Initialize the e-Paper register (Partial display)
+// parameter:
+// ******************************************************************************/
+// void EPD_2IN66_Init_partial(void)
+// {
+//     EPD_2IN66_Reset();
+//     EPD_2IN66_ReadBusy();
+//     EPD_2IN66_SendCommand(0x12);//soft  reset
+//     EPD_2IN66_ReadBusy();
+   
+//     EPD_2IN66_SetLUA();
+//     EPD_2IN66_SendCommand(0x37); 
+//     EPD_2IN66_SendData(0x00);  
+//     EPD_2IN66_SendData(0x00);  
+//     EPD_2IN66_SendData(0x00);  
+//     EPD_2IN66_SendData(0x00); 
+//     EPD_2IN66_SendData(0x00);  	
+//     EPD_2IN66_SendData(0x40);  
+//     EPD_2IN66_SendData(0x00);  
+//     EPD_2IN66_SendData(0x00);   
+//     EPD_2IN66_SendData(0x00);  
+//     EPD_2IN66_SendData(0x00);
+
+// 	/* Y increment, X increment */
+//     EPD_2IN66_SendCommand(0x11);
+//     EPD_2IN66_SendData(0x03);
+//     /*	Set RamX-address Start/End position	*/
+//     EPD_2IN66_SendCommand(0x44);
+//     EPD_2IN66_SendData(0x01); 
+//     EPD_2IN66_SendData((EPD_2IN66_WIDTH % 8 == 0)? (EPD_2IN66_WIDTH / 8 ): (EPD_2IN66_WIDTH / 8 + 1) );
+//     /*	Set RamY-address Start/End position	*/
+//     EPD_2IN66_SendCommand(0x45);
+//     EPD_2IN66_SendData(0);
+//     EPD_2IN66_SendData(0);
+//     EPD_2IN66_SendData((EPD_2IN66_HEIGHT&0xff));
+//     EPD_2IN66_SendData((EPD_2IN66_HEIGHT&0x100)>>8);
+
+//     EPD_2IN66_SendCommand(0x3C); 
+//     EPD_2IN66_SendData(0x80);   
+
+//     EPD_2IN66_SendCommand(0x22); 
+//     EPD_2IN66_SendData(0xcf); 
+//     EPD_2IN66_SendCommand(0x20); 
+//     EPD_2IN66_ReadBusy();
+// }
+// */
